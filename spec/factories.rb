41455b412dbd3214694d1074e62d8b72c442a7f3
Adam Abed <adam.abed.tipjar@gmail.com>
2014-07-19 22:05:04 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Adam Abed"
-    email    "aaabed@example.com"
-    password "abba123456"
-    password_confirmation "abba123456"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
